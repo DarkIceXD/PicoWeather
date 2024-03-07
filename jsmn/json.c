fe6df173c49351e18dce4bdbdb04a497347394e8
DarkIceXD <15961071+DarkIceXD@users.noreply.github.com>
2024-03-07 15:42:07 +0100

    refactored json utilities

--- jsmn/json.c
+++ jsmn/json.c
@@ -1,23 +1,15 @@
 #include "jsmn.h"
 #include "json.h"
+#include <stdlib.h>
 #include <string.h>
 
-int get_json_tokens_needed(const char *json, const size_t len)
+struct parser_data
 {
-    jsmn_parser p;
-    jsmn_init(&p);
-    return jsmn_parse(&p, json, len, NULL, 0);
-}
+    void (*string_handler)(const char *json, const jsmntok_t *t, const size_t count, struct json_data *data);
+    struct json_data data;
+};
 
-void parser_init(struct parser_data *data, void (*string_handler)(const char *json, const jsmntok_t *t, const size_t count, struct json_data *data), void *user_data)
-{
-    data->string_handler = string_handler;
-    data->data.array_depth = 0;
-    data->data.array_index = 0;
-    data->data.data = user_data;
-}
-
-int json_parse(const char *json, const jsmntok_t *t, const size_t count, struct parser_data *data)
+static int dump_json(const char *json, const jsmntok_t *t, const size_t count, struct parser_data *data)
 {
     if (count == 0)
         return 0;
@@ -38,9 +30,9 @@ int json_parse(const char *json, const jsmntok_t *t, const size_t count, struct 
         for (int i = 0; i < t->size; i++)
         {
             const jsmntok_t *key = t + 1 + j;
-            j += json_parse(json, key, count - j, data);
+            j += dump_json(json, key, count - j, data);
             if (key->size > 0)
-                j += json_parse(json, t + 1 + j, count - j, data);
+                j += dump_json(json, t + 1 + j, count - j, data);
         }
         return j + 1;
     }
@@ -51,12 +43,33 @@ int json_parse(const char *json, const jsmntok_t *t, const size_t count, struct 
         for (int i = 0; i < t->size; i++)
         {
             data->data.array_index = i;
-            j += json_parse(json, t + 1 + j, count - j, data);
+            j += dump_json(json, t + 1 + j, count - j, data);
         }
         data->data.array_depth--;
         return j + 1;
     }
     return 0;
+}
+
+bool json_parse(const char *json, void (*string_handler)(const char *json, const jsmntok_t *t, const size_t count, struct json_data *data), void *user_data)
+{
+    const int json_len = strlen(json);
+    jsmn_parser p;
+    jsmn_init(&p);
+    const int count = jsmn_parse(&p, json, json_len, NULL, 0);
+    if (count < 1)
+        return false;
+    jsmntok_t *t = malloc(sizeof(jsmntok_t) * count);
+    jsmn_init(&p);
+    jsmn_parse(&p, json, json_len, t, count);
+    struct parser_data data;
+    data.string_handler = string_handler;
+    data.data.array_depth = 0;
+    data.data.array_index = 0;
+    data.data.data = user_data;
+    dump_json(json, t, count, &data);
+    free(t);
+    return true;
 }
 
 bool is_json_key(const char *json, const jsmntok_t *tok, const char *key)
--- jsmn/json.h
+++ jsmn/json.h
@@ -11,15 +11,7 @@ struct json_data
     void *data;
 };
 
-struct parser_data
-{
-    void (*string_handler)(const char *json, const jsmntok_t *t, const size_t count, struct json_data *data);
-    struct json_data data;
-};
-
-int get_json_tokens_needed(const char *json, const size_t len);
-void parser_init(struct parser_data *data, void (*string_handler)(const char *json, const jsmntok_t *t, const size_t count, struct json_data *data), void *user_data);
-int json_parse(const char *json, const jsmntok_t *t, const size_t count, struct parser_data *data);
+bool json_parse(const char *json, void (*string_handler)(const char *json, const jsmntok_t *t, const size_t count, struct json_data *data), void *user_data);
 bool is_json_key(const char *json, const jsmntok_t *tok, const char *key);
 
 #endif
